0d8323f3bae82dadb482f2cf3d726f1eaed417b5
Guilherme Totti <dev.guilhermetotti@gmail.com>
2025-03-23 10:26:02 -0300

    feat: set base radix themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { Box } from "@radix-ui/themes";
+import Header from "./components/Header";
+import Gallery from "./components/Gallery";
+
 function App() {
-  return <h1>Hello world!</h1>;
+  return (
+    <Box maxWidth="80rem" mx="auto">
+      <Header />
+      <Gallery />
+    </Box>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "@radix-ui/themes/styles.css";
+import App from "./App.tsx";
+import { Theme } from "@radix-ui/themes";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Theme appearance="light" accentColor="yellow" grayColor="slate">
+      <App />
+    </Theme>
+  </StrictMode>
+);
